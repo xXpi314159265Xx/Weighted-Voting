c5f76bbe9bb7ddf10b740cabb67e950f11575e6b
xXpi314159265Xx <itd07@yahoo.com>
2016-11-13 18:14:37 +0100

    Added main function and improved output.

--- Shapley-Shubik Power Index.py
+++ Shapley-Shubik Power Index.py
@@ -59,6 +59,24 @@ def find_ratios(dict):
 		print("Player %s has %i/%i, or %.2f%% of the power" %(i,dict["player " + str(i)],total_critical,percent))
 	return
 
-quota, weights, player_list, player_score = get_data()
-winners = winning_coalitions(quota, weights, player_list)
-player_scores_dict = print(critical_players(winners,weights,quota,player_score))
+
+
+def main():
+	'''Calculates the Banzhaf Power Distribution of a weighted
+	voting system.'''
+	print()
+	print("The Yaiko Shapley-Shubik Power Distribution Calculator")
+	print("Version 1.1.3")
+	print()
+	quota, weights, player_list, player_score = get_data()
+	winners = winning_coalitions(quota, weights, player_list)
+	#print()
+	#print("WINNING COALTIONS")
+	#for i in range(len(winners)):
+	#	print(str(winners[i]))
+	#print()
+	player_scores_dict = critical_players(winners,weights,quota,player_score)
+	print("BANZHAF POWER INDEX")
+	find_ratios(player_scores_dict)
+
+main()
