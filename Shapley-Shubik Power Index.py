9bf3602f9a536599fd8c8cc3a6f1c40003e10ac4
xXpi314159265Xx <itd07@yahoo.com>
2016-11-13 18:30:38 +0100

    Improved readability of variables and improved output.

--- Shapley-Shubik Power Index.py
+++ Shapley-Shubik Power Index.py
@@ -33,6 +33,7 @@ def convert_to_number(list, dict):
 def pivotal_players(list, dict, quota, player_scores):
 	'''Takes a list of coalitions, a dictionary that converts players to their weights and finds the pivotal player. Returns dictionary with number of times each player is pivotal.'''
 	total = 0
+	pivotal_list = []
 	for i in list:
 		num_list = convert_to_number(i, dict)
 		for j in range(len(num_list)):
@@ -41,8 +42,9 @@ def pivotal_players(list, dict, quota, player_scores):
 			if total >= quota:
 				player_scores[i[j]] += 1
 				total = 0
+				pivotal_list.append(i[j])
 				break
-	return player_scores
+	return player_scores, pivotal_list
 
 def find_ratios(dict):
 	'''Takes a dictionary of players and number of time player is critical
@@ -62,13 +64,14 @@ def main():
 	print()
 	quota, weights, player_list, player_score = get_data()
 	coalition = coalitions(player_list)
-	#print()
-	#print("WINNING COALTIONS")
-	#for i in range(len(winners)):
-	#	print(str(winners[i]))
-	#print()
-	player_scores_dict = pivotal_players(coalition,weights,quota,player_score)
+	player_scores_dict, pivotal_list = pivotal_players(coalition,weights,quota,player_score)
+	print()
+	print("COALTIONS WITH PIVOTAL PLAYER")
+	for i in range(len(coalition)):
+		print(str(coalition[i]) + "     " + str(pivotal_list[i]))
+	print()
 	print("BANZHAF POWER INDEX")
 	find_ratios(player_scores_dict)
+	print(pivotal_list)
 
 main()
